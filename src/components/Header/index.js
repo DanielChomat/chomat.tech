399690d2034a966694b3461beba3bdd190644dd9
Daniel Chomát <daniel.chomat@refresh.cz>
2020-07-01 11:12:08 +0200

    Editing the basic layout

---
+++ src/components/Header/index.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { rhythm, scale } from "../../utils/typography"
+import { Link } from "gatsby"
+
+const Header = ({ location, title }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+
+  if (location.pathname === rootPath) {
+    header = (
+      <h1
+        style={{
+          ...scale(1.5),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+
+  return <header>{header}</header>
+}
+
+export default Header
