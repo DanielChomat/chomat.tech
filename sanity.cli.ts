8be048be6d0975578b7c56b0f835963b6a8b84db
Daniel Chomát <daniel@chomat.eu>
2023-10-27 18:23:26 +0200

    chore(Sanity Studio): Moving Studio into root & updating dependencies

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import { SANITY_STUDIO_DATASET, SANITY_STUDIO_PROJECT_ID } from "./src/environment";
+import { defineCliConfig } from "sanity/lib/exports/cli";
+
+export default defineCliConfig({
+  api: {
+    projectId: SANITY_STUDIO_PROJECT_ID,
+    dataset: SANITY_STUDIO_DATASET,
+  },
+})
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig, InferSchemaValues } from "@sanity-typed/types";
 import { visionTool } from "@sanity/vision";
-import { schemaTypes } from "./schemas/schema";
+import { schemaTypes } from "./src/schemas/schema";
 import { isDev } from "sanity";
+import { SANITY_STUDIO_DATASET, SANITY_STUDIO_PROJECT_ID, SANITY_STUDIO_TITLE } from "./src/environment";
 
 const devOnlyPlugins = [visionTool()]
 
+console.log({SANITY_STUDIO_TITLE});
+console.log(process.env);
+
 const config = defineConfig({
-  title: "chomat.tech",
-  projectId: "et8ux3we",
-  dataset: "chomat",
+  title: SANITY_STUDIO_TITLE,
+  projectId: SANITY_STUDIO_PROJECT_ID,
+  dataset: SANITY_STUDIO_DATASET,
   plugins: [...(isDev ? devOnlyPlugins : []), visionTool()],
   schema: {
     types: schemaTypes,
